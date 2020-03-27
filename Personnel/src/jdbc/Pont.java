4e99cf0e119b312b41fd5d6be148211c5f665db7
Alexis <alexis.vatrinet@hotmail.fr>
2020-03-27 17:34:05 +0100

    Nom de la base changé

--- Personnel/src/jdbc/Pont.java
+++ Personnel/src/jdbc/Pont.java
@@ -6,7 +6,7 @@ public class Pont {
 			driverClassName = "com.mysql.cj.jdbc.Driver",
 			host = "localhost", 
 			port ="3306",
-			database ="ppe",
+			database ="personnel",
 			user = "root",
 			password = "";
 
